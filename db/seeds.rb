d2aa72facda5f762294ee1fa5f5d01a8084f7087
Bill Bawden <wabawden@aol.com>
2020-08-18 14:37:15 +0100

    seed database with users and chefs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "creating 20 users"
+20.times do
+  user = User.new(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      phone_number: Faker::PhoneNumber.phone_number,
+      address: Faker::Address.street_address,
+      postcode: Faker::Address.postcode,
+      is_a_chef: [true, false].sample,
+      email: Faker::Internet.free_email,
+      password: "123456"
+  )
+  user.save!
+  puts "saved #{user.first_name}"
+end
+
+
+puts "assigning chef attributes to chefs"
+User.where(is_a_chef: true).each do |user|
+   chef = Chef.create(
+       description: Faker::Hipster.sentences,
+       cuisine: Faker::Nation.nationality,
+       location_range: [5, 10, 20, 30, 50].sample,
+       price: [5, 10, 20, 30, 50].sample,
+       chef_postcode: Faker::Address.postcode
+   )
+    chef.user = user
+    chef.save!
+    puts "saved chef attributes for #{chef.user.first_name}"
+
+end
