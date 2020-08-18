8208f9ba75d30291844d1c45bded56354b94d7f0
Bill Bawden <wabawden@aol.com>
2020-08-18 15:41:51 +0100

    edit user edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_many :reviews, through: :bookings
   has_many :messages
   has_one :chef, dependent: :destroy
+  has_one_attached :photo
   validates :first_name, :last_name, :phone_number, :address, :postcode, presence: :true
 end
